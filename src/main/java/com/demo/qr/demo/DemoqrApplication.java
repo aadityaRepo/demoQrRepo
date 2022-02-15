bad8f1458d155b3cc712bdda251704ccf31b1ab1
naveennarayanappa1 <nnaveen840@gmail.com>
2022-02-15 14:32:24 +0530

    this is my first commit please take it

---
+++ src/main/java/com/demo/qr/demo/DemoqrApplication.java
@@ -0,0 +1,13 @@
+package com.demo.qr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoqrApplication.class, args);
+	}
+
+}
