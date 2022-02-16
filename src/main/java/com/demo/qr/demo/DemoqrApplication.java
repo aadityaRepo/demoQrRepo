2ca42a20e99b4df6fd8162c2cfa165abda619709
amitky29 <57131871+amitky29@users.noreply.github.com>
2022-02-16 11:11:02 +0530

    Update DemoqrApplication.java
    Amit K Yadav checked in

--- src/main/java/com/demo/qr/demo/DemoqrApplication.java
+++ src/main/java/com/demo/qr/demo/DemoqrApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoqrApplication {
 // This is Dhruv checking in
+	// Amit K Yadav checking in
 	public static void main(String[] args) {
 		//aaditya changes
 	        SpringApplication.run(DemoqrApplication.class, args);
