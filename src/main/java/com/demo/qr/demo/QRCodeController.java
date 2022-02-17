b17767fa6907f357cd79223f2c91e8dede92b33f
akash <aaditya.singh.01712@gmail.com>
2022-02-17 16:00:10 +0530

    Adding the new file

---
+++ src/main/java/com/demo/qr/demo/QRCodeController.java
@@ -0,0 +1,28 @@
+package com.demo.qr.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class QRCodeController {
+	
+	private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
+	private final int WIDTH = 300;
+	private final int HEIGHT = 300;
+	private final String QR_TEXT = "Spring Boot REST API to generate QR Code - Websparrow.org";
+
+
+
+	@GetMapping("/greeting")
+	public ModelAndView greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		ModelAndView mav = new ModelAndView("developer");
+		Developer dev = new Developer();
+		mav.addObject("developer", dev);
+		return mav;
+	}
+
+
+
+}
