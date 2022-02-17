24284deb8de1413bfceb40d455ff7410667a13c6
akash <aaditya.singh.01712@gmail.com>
2022-02-17 15:49:38 +0530

    Dependancy Changes

--- src/main/java/com/demo/qr/demo/DemoqrApplication.java
+++ src/main/java/com/demo/qr/demo/DemoqrApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoqrApplication {
 // This is Dhruv checking in
 	public static void main(String[] args) {
-		//aaditya changes
-		//naveen changes done
-		//changed by nancy
-	        SpringApplication.run(DemoqrApplication.class,args);
+		      SpringApplication.run(DemoqrApplication.class,args);
 	}
 
 }
